d71808ed6dafb14be15e8ac637aa95427aab2599
System Administrator <admin@example.com>
2019-10-30 13:12:13 -0400

    Changes by 'System Administrator' on 'CICD Test - 1.0.0' -- Build #1

---
+++ sn/CICD Test/Dictionary Entry/x_308211_cicd_test_test_request/active_true/collection/calculation.js
@@ -0,0 +1,15 @@
+/* 
+ * Application : CICD Test
+ * ClassName   : sys_dictionary
+ * Created On  : 2019-10-30 17:10:54
+ * Created By  : admin
+ * Updated On  : 2019-10-30 17:10:54
+ * Updated By  : admin
+ * URL         : /sys_dictionary.do?sys_id=c791f275dbf00010343f58b3ca96196a
+ */
+(function calculatedFieldValue(current) {
+
+	// Add your code here
+	return '';  // return the calculated value
+
+})(current);
